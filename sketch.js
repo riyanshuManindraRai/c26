ce43efe082c308b674eb432ef1a70177ff319560
riyanshuManindraRai <rai.priya198712@gmail.com>
2021-05-12 16:44:50 +0530

    hi

--- sketch.js
+++ sketch.js
@@ -17,6 +17,7 @@ function setup(){
 
     
     ground = new Ground(600,height,1200,20)
+    ground = new Ground(600,height,200,20)
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
